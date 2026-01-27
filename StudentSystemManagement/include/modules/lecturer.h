4368aca22926398dcad81e4c4faf479b1314b0f4
duongnpb5482-oss <duongnpb5482@ut.edu.vn>
2026-01-27 22:24:43 +0700

    Update lecturer.h

--- StudentSystemManagement/include/modules/lecturer.h
+++ StudentSystemManagement/include/modules/lecturer.h
@@ -1,1 +1,33 @@
+#ifndef LECTURER_H
+#define LECTURER_H
+
+#include <string>
+
+class Lecturer {
+private:
+    std::string id;
+    std::string name;
+    std::string email;
+    std::string phone;
+    std::string department;
+
+public:
+    Lecturer();
+    Lecturer(
+        const std::string& id,
+        const std::string& name,
+        const std::string& email,
+        const std::string& phone,
+        const std::string& department
+    );
+
+    // Getter
+    std::string getId() const;
+    std::string getName() const;
+    std::string getEmail() const;
+    std::string getPhone() const;
+    std::string getDepartment() const;
+};
+
+#endif
 
