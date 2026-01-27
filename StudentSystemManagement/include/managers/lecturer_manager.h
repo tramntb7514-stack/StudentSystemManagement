1f690a5d8df5cca31d76c71b12bedd3c44f7e974
duongnpb5482-oss <duongnpb5482@ut.edu.vn>
2026-01-27 22:25:28 +0700

    Update lecturer_manager.h

--- StudentSystemManagement/include/managers/lecturer_manager.h
+++ StudentSystemManagement/include/managers/lecturer_manager.h
@@ -1,1 +1,22 @@
+#ifndef LECTURER_MANAGER_H
+#define LECTURER_MANAGER_H
+
+#include <vector>
+#include <iostream>
+#include "../modules/lecturer.h"
+
+class LecturerManager {
+private:
+    std::vector<Lecturer> lecturers;
+
+public:
+    // CRUD
+    void addLecturer(const Lecturer& lecturer);
+    bool removeLecturerById(const std::string& id);
+    Lecturer* findLecturerById(const std::string& id);
+
+    void showAllLecturers() const;
+};
+
+#endif
 
