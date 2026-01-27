1e22c5da5f23b11851cecb476d56a2e7130b10c3
tramntb7514-stack <tramntb7514@ut.edu.vn>
2026-01-27 23:18:47 +0700

    ui.h

--- StudentSystemManagement/include/core/ui.h
+++ StudentSystemManagement/include/core/ui.h
@@ -1,1 +1,20 @@
+#ifndef UI_H
+#define UI_H
+
+#include <iostream>
+using namespace std;
+
+inline void showMainMenu() {
+    cout << "=============================\n";
+    cout << "  STUDENT MANAGEMENT SYSTEM\n";
+    cout << "=============================\n";
+    cout << "1. Student\n";
+    cout << "2. Lecturer\n";
+    cout << "3. Admin\n";
+    cout << "0. Exit\n";
+    cout << "-----------------------------\n";
+    cout << "Choose: ";
+}
+
+#endif
 
