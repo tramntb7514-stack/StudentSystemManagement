2a6712b9b6d4711bec0d9d1f90a95e2ec270b9f1
duongnpb5482-oss <duongnpb5482@ut.edu.vn>
2026-01-27 22:23:42 +0700

    Update student.h

--- StudentSystemManagement/include/modules/student.h
+++ StudentSystemManagement/include/modules/student.h
@@ -1,1 +1,33 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+
+class Student {
+private:
+    std::string id;
+    std::string name;
+    std::string email;
+    std::string phone;
+    std::string className;
+
+public:
+    Student();
+    Student(
+        const std::string& id,
+        const std::string& name,
+        const std::string& email,
+        const std::string& phone,
+        const std::string& className
+    );
+
+    // Getter
+    std::string getId() const;
+    std::string getName() const;
+    std::string getEmail() const;
+    std::string getPhone() const;
+    std::string getClassName() const;
+};
+
+#endif
 
