72bd77b44456cd2123878ec2ac4bf7f478c3404b
duongnpb5482-oss <duongnpb5482@ut.edu.vn>
2026-01-27 22:25:06 +0700

    Update student_manager.h

--- StudentSystemManagement/include/managers/student_manager.h
+++ StudentSystemManagement/include/managers/student_manager.h
@@ -1,1 +1,22 @@
+#ifndef STUDENT_MANAGER_H
+#define STUDENT_MANAGER_H
+
+#include <vector>
+#include <iostream>
+#include "../modules/student.h"
+
+class StudentManager {
+private:
+    std::vector<Student> students;
+
+public:
+    // CRUD
+    void addStudent(const Student& student);
+    bool removeStudentById(const std::string& id);
+    Student* findStudentById(const std::string& id);
+
+    void showAllStudents() const;
+};
+
+#endif
 
